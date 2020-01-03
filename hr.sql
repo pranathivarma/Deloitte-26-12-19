47af52334b8e763d24276b220ae794fc50bef69f
pranathivarma <40718330+pranathivarma@users.noreply.github.com>
2020-01-03 17:41:31 +0530

    Add files via upload

--- hr.sql
+++ hr.sql
@@ -198,12 +198,810 @@ FROM summary
 WHERE dept_total > (SELECT SUM(dept_total) * 1/8
 FROM summary);
 
+SELECT employee_id, job_id
+FROM employees
+UNION
+SELECT employee_id, job_id
+FROM job_history;
 
-        
+SELECT employee_id, job_id
+FROM employees
+UNION ALL
+SELECT employee_id, job_id
+FROM job_history
+ORDER BY employee_id;      
+                                    
+SELECT employee_id, job_id
+FROM employees
+INTERSECT
+SELECT employee_id, job_id
+FROM job_history
+ORDER BY employee_id;     
+
+SELECT employee_id, job_id
+FROM employees
+MINUS
+SELECT employee_id, job_id
+FROM job_history
+ORDER BY employee_id;    
                    
-   
-   
+SELECT department_id,TO_NUMBER(null) location,hire_date
+FROM employees
+UNION 
+SELECT department_id,location_id,TO_DATE(null)
+FROM departments;
                                      
+SELECT location_id,department_id
+FROM departments
+UNION 
+SELECT location_id,TO_NUMBER(NULL)
+FROM locations;
+
+SELECT employee_id,job_id,salary
+FROM employees
+UNION
+SELECT employee_id,job_id,0
+FROM job_history;
+
+COLUMN a_dummy NOPRINT
+SELECT 'sing' AS "My dream",3 a_dummy
+FROM dual
+UNION
+SELECT 'I''d like to teach',1 a_dummy
+FROM dual
+UNION
+SELECT 'the world to',2 a_dummy
+FROM dual
+ORDER BY a_dummy;
+
+INSERT INTO departments(department_id,department_name,manager_id,location_id)
+VALUES(71,'Public Relations',100,1700);
+
+INSERT INTO departments (department_id,department_name  )
+VALUES (31,'Purchasing');
+
+INSERT INTO departments
+VALUES   (271,'Finance',NULL,NULL);
+
+INSERT INTO employees (employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,commission_pct,manager_id,department_id)
+VALUES(272,'Raju','Prem','Rajesh','pranathivarma@gmail.com','9848563421',SYSDATE,'AC_ACCOUNT',6900,NULL,205,71);
+
+INSERT INTO employees
+VALUES (114,'Den', 'Raphealy','DRAPHEAL','pranathivarma@gmail.com','9848563421', '515.127.4561',TO_DATE('FEB 3, 1999', 'MON DD, YYYY'),'SA_REP', 11000, 0.2, 100, 60);
+
+INSERT INTO departments(department_id, department_name, location_id)
+VALUES (&department_id, '&department_name',&location);
+
+INSERT INTO sales_reps(id,name,salary,commission_pct)
+SELECT employee_id,last_name,salary,commission_pct
+FROM employees
+WHERE job_id LIKE '%REP%';
+
+UPDATE employees
+SET department_id = 50
+WHERE employee_id = 113;
+
+UPDATE employees
+SET job_id = (SELECT job_id FROM employees WHERE employee_id = 205),
+    salary = (SELECT salary FROM employees WHERE employee_id = 205)
+WHERE employee_id = 114;
+
+DELETE FROM employees
+WHERE department_id= (SELECT department_id FROM departments WHERE department_id LIKE '%71');
+
+INSERT INTO locations (location_id,
+street_address,postal_code,city,state_province,country_id)
+VALUES (3300,'3/1 Ali Street','500008','Hyderabad','Telengana','IN');
+
+SELECT * FROM locations;
+
+INSERT INTO departments (department_id,
+department_name, manager_ID, location_id)
+VALUES (300,'Analytics',150, 3300);
+
+SELECT * FROM departments;
+
+INSERT INTO employees (employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,commission_pct,manager_id,department_id)
+VALUES(207,'Pranathi','Varma','pranathivarma@gmail.com','96471239',SYSDATE,'AC_ACCOUNT',50000,0.05,150,300);
+
+SELECT * FROM employees;
+
+UPDATE employees
+SET salary = 50000+(0.5*50000)
+WHERE salary = 50000 AND employee_id=207;
+
+DELETE FROM employees
+WHERE employee_id=207 AND first_name='Pranathi'; 
+
+DELETE FROM departments
+WHERE department_id=300 AND department_name='Analytics';
+
+SELECT * FROM employees WHERE department_id=50;
+
+INSERT INTO (SELECT employee_id,first_name,last_name,email,phone_number,hire_date,job_id,salary,commission_pct,manager_id,department_id 
+             FROM employees
+             WHERE department_id = 50)
+VALUES (99999,'taylor','DTaylor','taylor@gmail.com',2589631472,TO_DATE('07-JUN-99','DD-MON-RR'),'ST_MAN',50000,0.5,120,50);
+
+select * FROM locations;
+
+UPDATE locations
+SET city='Mysore'
+WHERE location_id=3300 AND country_id='IN';
+
+SAVEPOINT update_done;
+
+INSERT INTO locations(location_id,street_address,postal_code,city,state_province,country_id)
+VALUES (3500,'3/1 Ali baig Street','500008','Bangalore','Telengana','IN');
+
+ROLLBACK TO update_done;
+
+SELECT * FROM locations;
+
+DELETE FROM locations
+WHERE location_id = 3400;
+
+DELETE FROM employees
+WHERE employee_id=99999;
+
+CREATE TABLE dept
+            (deptno  NUMBER(2),
+            dname VARCHAR2(14),
+            loc   VARCHAR2(13),
+            create_date DATE DEFAULT SYSDATE);
+            
+SELECT * FROM dept;
+
+CREATE TABLE dept80
+  AS
+    SELECT employee_id,last_name,
+           salary*12 ANNSAL,
+           hire_date
+    FROM   employees
+    WHERE department_id = 80;
+    
+DESCRIBE dept80;
+
+CREATE TABLE copyEmps
+  AS 
+    SELECT * FROM employees;
+    
+SELECT * FROM copyEmps;
+
+DELETE FROM copyEmps;
+
+ROLLBACK;
+
+CREATE TABLE ROLES(
+    RoleID number(5)
+    constraint roleid_pk PRIMARY KEY,
+    RoleName varchar(255)
+    constraint rolename_nn NOT NULL
+    );
+
+CREATE TABLE USERS (
+    UserID number(5)
+    constraint userid_pk PRIMARY KEY,
+    FName varchar(255)
+    constraint fname_nn NOT NULL,
+    UserName varchar2(10)
+    constraint username_nn NOT NULL
+    constraint username_uk UNIQUE,
+    Password varchar2(255)
+    constraint password_nn CHECK(LENGTH(Password)>=8),
+    EmailId varchar2(255)
+    constraint email_nn NOT NULL
+    constraint email_uk UNIQUE,
+    RoleId number(5)
+    constraint roleid_fk REFERENCES
+    ROLES (RoleId)
+);
+
+INSERT INTO ROLES (RoleId, RoleName)
+VALUES (301, 'User');
+
+INSERT INTO ROLES (RoleId, RoleName)
+VALUES (302, 'Admin');
+
+INSERT INTO ROLES (RoleId, RoleName)
+VALUES (303, 'manager');
+
+
+INSERT INTO USERS (UserId,FName,USERNAME, PASSWORD, EMAILID, ROLEID)
+VALUES (33,'Priya','Priya96','5874396158','priya4432@gmail.com',301);
+
+INSERT INTO USERS (UserId,FName,USERNAME, PASSWORD, EMAILID, ROLEID)
+VALUES (32,'Hari','har86','8745120039','har4432@gmail.com',302);
+
+INSERT INTO USERS (UserId,FName,USERNAME, PASSWORD, EMAILID, ROLEID)
+VALUES (34,'Ansar','ansar45','5846921371','ansar75@gmail.com',303);
+
+INSERT INTO USERS (UserId,FName,USERNAME, PASSWORD, EMAILID, ROLEID)
+VALUES (35,'Reshma','Reshma21','2589631473','reshma75@gmail.com',301);
+
+INSERT INTO USERS (UserId,FName,USERNAME, PASSWORD, EMAILID, ROLEID)
+VALUES (36,'vinod','vinod56','7894561235','vinod74@gmail.com',302);
+
+INSERT INTO USERS (UserId,FName,USERNAME, PASSWORD, EMAILID, ROLEID)
+VALUES (37,'Yuktha','yuktha98','85245691735','yuktha@gmail.com',303);
+
+CREATE VIEW empvu80
+AS SELECT employee_id, last_name, salary
+FROM employees
+WHERE department_id = 80;
+
+DESCRIBE empvu80
+
+CREATE OR REPLACE VIEW empvu80
+(id_number, name, sal, department_id)
+AS SELECT employee_id, first_name || ' '
+|| last_name, salary, department_id
+FROM employees
+WHERE department_id = 80;
+
+CREATE OR REPLACE VIEW dept_sum_vu
+(name, minsal, maxsal, avgsal)
+AS SELECT d.department_name, MIN(e.salary),
+MAX(e.salary),AVG(e.salary)
+FROM employees e JOIN departments d
+ON (e.department_id = d.department_id)
+GROUP BY d.department_name;
+
+CREATE OR REPLACE VIEW empvu20
+AS SELECT *
+    FROM employees
+    WHERE department_id=20
+    WITH CHECK OPTION CONSTRAINT empvu20_ck;
+ 
+CREATE OR REPLACE VIEW empvu10
+   (employee_number,employee_name,job_title)
+AS SELECT  employee_id,last_name,job_id
+     FROM employees
+     WHERE department_id = 10
+     WITH READ ONLY;
+     
+CREATE SEQUENCE users_id_seq
+                INCREMENT BY 10
+                START WITH 10
+                MAXVALUE 9999
+                NOCACHE
+                NOCYCLE;
+                
+INSERT INTO roles(roleid,rolename)
+VALUES   (1,'support');
+
+SELECT * FROM ROLES
+
+INSERT INTO roles(roleid,rolename)
+VALUES   (1,'matter');
+    
+CREATE INDEX users_fname_idx
+ON users(fname);
+
+DESCRIBE DICTIONARY
+
+SELECT * 
+FROM dictionary
+
+SELECT object_name,object_type,created,status
+FROM user_objects
+ORDER BY object_type;
+
+DESCRIBE user_tables
+
+SELECT table_name
+FROM user_tables;
+
+DESCRIBE user_tab_columns;
+
+SELECT column_name,data_type,data_length,data_precision,data_scale,nullable
+FROM user_tab_columns
+WHERE table_name = 'USERS';
+
+DESCRIBE user_constraints;
+
+SELECT constraint_name,constraint_type,search_condition,r_constraint_name,delete_rule,status
+FROM user_constraints
+WHERE table_name='USERS';
+
+DESCRIBE user_cons_columns;
+
+SELECT constraint_name,column_name
+FROM user_cons_columns
+WHERE table_name='USERS';
+
+SELECT DISTINCT view_name FROM user_views;
+
+SELECT text FROM user_views
+WHERE view_name = 'EMPVU80';
+
+SELECT sequence_name,min_value,max_value,increment_by,last_number
+FROM user_sequences;
+
+SELECT * FROM user_synonyms;
+
+COMMENT ON TABLE users
+IS'Users Information';
+
+SELECT * FROM USERS_TAB_COMMENTS;
+
+SELECT * FROM USERS;
+select * from roles;
+
+select u.fname,u.username,u.emailid,r.rolename from users u,roles r 
+				where u.roleid=r.roleid
+                
+select u.fname,u.username,u.emailid,r.rolename from users u,roles r where u.roleid=r.roleid;                
+
+commit;
+
+set serveroutput on;
+DECLARE
+  a NUMBER := 4;
+BEGIN
+  a := a+1;
+  if mod(a,2)=0 then
+    DBMS_output.put_line(a); 
+  else
+    DBMS_output.put_line(a+1); 
+  end if;
+END;
+/
+
+DECLARE
+  i NUMBER := 1;
+BEGIN
+  LOOP
+    INSERT INTO T1 VALUES(i,i);
+    i := i+1;
+    EXIT WHEN i>100;
+  END LOOP;
+END;
+/
+
+SELECT * FROM T1;
+  
+DECLARE
+  i NUMBER := 1;
+BEGIN
+  WHILE i<100 LOOP
+    INSERT INTO T1 VALUES(i,i);
+    i := i+2;
+  END LOOP;
+END;
+/
+
+SELECT * FROM T1;
+
+DECLARE
+  a NUMBER := 1;
+  i NUMBER :=1;
+BEGIN
+  FOR i IN 1..5 LOOP
+    INSERT INTO T1 VALUES(a,a);
+    a:=a+1;
+  END LOOP;
+END;
+/
+    
+SET SERVEROUTPUT ON SIZE 1000000;
+DECLARE
+ n_pct employees.commission_pct%TYPE;
+ v_eval varchar2(10);
+ n_emp_id employees.employee_id%TYPE := 145;
+BEGIN
+ --get commission percentage
+ SELECT commission_pct
+ INTO n_pct
+ FROM employees
+ WHERE employee_id = n_emp_id;
+ --evaluate commission percentage
+ CASE n_pct
+  WHEN 0 THEN
+    v_eval := 'N/A';
+  WHEN 0.1 THEN
+    v_eval := 'Low';
+  WHEN 0.4 THEN
+    v_eval := 'High';
+  ELSE
+    v_eval := 'Fair';
+ ENd CASE;
+ --print commission evaluation
+ DBMS_OUTPUT.PUT_LINE('Employee'||n_emp_id||'commission'|| TO_CHAR(n_pct) ||
+                       ' which is '   || v_eval);
+END;
+/  
+
+SET SERVEROUTPUT ON SIZE 1000000;
+DECLARE
+ n_name USERS.FNAME%TYPE := 'Yuktha';
+ v_eval VARCHAR2(20);
+BEGIN
+ SELECT r.rolename
+ INTO v_eval
+ FROM users u,roles r
+ WHERE u.roleid=r.roleid AND u.fname=n_name;
+ DBMS_OUTPUT.PUT_LINE(v_eval);
+END;
+/
+
+CASE n_pct
+  WHEN 'Reshma' THEN
+    v_eval := '301';
+  WHEN 'vinod' THEN
+    v_eval := '302';
+  WHEN 'Yuktha' THEN
+    v_eval := '303';
+  WHEN 'Hari' THEN
+    v_eval := '301';
+  WHEN 'Priya' THEN
+    v_eval := '301';
+  WHEN 'Ansar' THEN
+    v_eval := '303';
+  ELSE 
+    v_eval := 'User does not exist'
+ ENd CASE;
+  
+SELECT * FROM ROLES; 
+
+DECLARE
+  i NUMBER := 1;
+BEGIN
+  LOOP
+    INSERT INTO T1 VALUES(i,i+1);
+    i := i+1;
+    EXIT WHEN i>100;
+  END LOOP;
+END;
+/
+
+SET SERVEROUTPUT ON SIZE 100000;
+DECLARE
+--declare a cursor
+ CURSOR cur_chief IS
+     SELECT last_name,hire_date,salary
+     FROM copyemps
+     where Extract(YEAR FROM hire_date)>='05';
+  r_chief cur_chief%ROWTYPE;
+ 
+ BEGIN
+  OPEN cur_chief;
+  LOOP
+   FETCH cur_chief INTO r_chief;
+   EXIT WHEN cur_chief%NOTFOUND;
+
+   
+   UPDATE copyemps SET salary = r_chief.salary+(0.1*r_chief.salary) ;
+   
+  END LOOP;
+END; 
+ /
+ 
+ SELECT * FROM copyemps;
+ 
+
+
+
+SET SERVEROUTPUT ON SIZE 100000;
+DECLARE
+--declare a cursor
+ CURSOR cur_chief IS
+     SELECT first_name,last_name
+     FROM copyemps 
+     Order by hire_date;
+  r_chief cur_chief%ROWTYPE;
+ 
+ BEGIN
+  OPEN cur_chief;
+  LOOP
+   FETCH cur_chief INTO r_chief;
+   EXIT WHEN cur_chief%NOTFOUND;
+
+   SELECT *
+     FROM copyemps 
+     Order by hire_date;
+     
+     DBMS_OUTPUT.PUT_LINE(r_chief.first_name|| ' ' ||r_chief.last_name);
+   
+  END LOOP;
+END; 
+ /
+ 
+
+
+DECLARE
+ oldest_hire_date date;
+ lname employee.last_name%TYPE;
+ fname employee.first_name%TYPE;
+ cursor find_oldest_hire_date is select min(hire_date) from employee;
+ cursor id_employee is select last_name,first_name from employees where hire_date=oldest_hire_date;
+ begin
+  open find_oldest_hire_date;
+  fetch find_oldest_hire_date into oldest_hire_date;
+  close find_oldest_hire_date;
+  open id_employee
+  fetch id_employee into lname,fname;
+  close id_employee;
+ dbms_output.put_line('the oldest employee is' || lname || ' '||fname);
+end;
+/  
+
+DECLARE 
+n_pct employees.commission_pct%TYPE;
+v_eval varchar2(10);
+n_emp_id employees.employee_id%TYPE := 145;
+
+BEGIN
+
+SELECT commission_pct
+INTO n_pct
+FROM employees
+WHERE employee_id = n_emp_id;
+
+CASE n_pct
+    WHEN 0 THEN
+    v_eval:='N/A';
+    WHEN 0.1 THEN
+    v_eval:='Low';
+    WHEN 0.4 THEN
+    v_eval:='High';
+    ELSE
+    v_eval:='Fair';
+    END CASE;
+    
+    DBMS_OUTPUT.PUT_LINE('EMPLOYEE '||n_emp_id||' commission is '|| TO_CHAR(n_pct)||' which is '||v_eval);
+    
+    END;
+    /
+    
+declare
+begin
+INSERT INTO employee_salary values(6,'Sri',53000,4000,1000,5000,15000,58000);
+declare 
+ename employee_salary.emp_name%TYPE:='&name';
+tax_percent NUMBER:=0;
+tax NUMBER:=0;
+grossSal employee_salary.gross_salary%TYPE:=0;
+BEGIN
+select gross_salary into grossSal from employee_salary where emp_name=ename;
+
+case 
+when (grossSal>=15000 and grossSal<25000) then
+tax_percent:=5;
+when (grossSal>=25000 and grossSal<50000) then
+tax_percent:=10;
+when grossSal>=50000 then
+tax_percent:=20;
+end case;
+tax:=tax_percent*grossSal/100;
+dbms_output.put_line('Tax for '|| ename || ' is '||tax);
+END;
+end;
+/
+
+DECLARE
+  -- declare a cursor
+  CURSOR cur_chief IS
+      SELECT u.name,
+             r.role_name
+      FROM users u
+      INNER JOIN roles r ON u.role_id = r.role_id;
+ 
+  r_chief cur_chief%ROWTYPE;
+BEGIN
+  OPEN cur_chief;
+  LOOP
+    -- fetch information from cursor into record
+    FETCH cur_chief INTO r_chief;
+ 
+    EXIT WHEN cur_chief%NOTFOUND;
+ 
+    -- print department - chief
+    DBMS_OUTPUT.PUT_LINE(r_chief.name|| ' - ' ||
+                         r_chief.role_name);
+  END LOOP;
+  -- close cursor cur_chief
+  CLOSE cur_chief;
+END;
+/
+
+declare 
+cursor cur_chief is select a,b from t1 where a<b for update;
+r_chief cur_chief%ROWTYPE;
+temp t1.a%type;
+begin
+open cur_chief;
+loop
+fetch cur_chief into r_chief;
+exit when cur_chief%NOTFOUND;
+temp:=r_chief.a;
+update t1 set a=r_chief.b where b=r_chief.b;
+update t1 set b=temp where b=r_chief.b;
+
+end loop;
+end;
+/
+
+declare
+cursor cur_chief is select employee_id,first_name,salary,hire_date from employees where hire_date>TO_DATE('31-Dec-2004');
+r_chief cur_chief%ROWTYPE;
+begin
+OPEN cur_chief;
+loop
+fetch cur_chief into r_chief;
+exit when cur_chief%NOTFOUND;
+dbms_output.put_line(r_chief.first_name||q'['s]'||' updated salary is '||(r_chief.salary*0.1+r_chief.salary));
+
+end loop;
+end;
+/
+
+declare
+cursor cur_chief is select last_name,first_name,hire_date from employees;
+r_chief cur_chief%ROWTYPE;
+oldDate employees.hire_date%TYPE:=sysdate;
+first_name employees.first_name%TYPE;
+last_name employees.last_name%TYPE;
+begin
+open cur_chief;
+loop
+fetch cur_chief into r_chief;
+exit when cur_chief%NOTFOUND;
+if r_chief.hire_date<oldDate 
+then
+oldDate:=r_chief.hire_date;
+first_name:=r_chief.first_name;
+last_name:=r_chief.last_name;
+end if;
+end loop;
+dbms_output.put_line(first_name||' '||last_name||' is the oldest employee hired on '||oldDate);
+end; 
+/
+
+
+SET SERVEROUTPUT ON SIZE 1000000;
+DECLARE
+  r_emp employees%ROWTYPE;
+  n_emp_id  employees.employee_id%TYPE := 200;
+BEGIN
+  SELECT *
+  INTO r_emp
+  FROM employees
+  WHERE employee_id = n_emp_id;
+  -- print out the employee's first name
+  DBMS_OUTPUT.PUT_LINE(r_emp.first_name);
+END;
+/
+
+
+SET SERVEROUTPUT ON SIZE 1000000;
+DECLARE
+  TYPE t_name IS RECORD(
+     first_name employees.first_name%TYPE,
+     last_name  employees.last_name%TYPE
+  );
+  r_name   t_name; -- name record
+  n_emp_id employees.employee_id%TYPE := 200;
+BEGIN
+  SELECT first_name,
+         last_name
+  INTO r_name
+  FROM employees
+  WHERE employee_id = n_emp_id;
+  -- print out the employee's name
+  DBMS_OUTPUT.PUT_LINE(r_name.first_name || ',' || r_name.last_name );
+END;
+/
+
+SET SERVEROUTPUT ON SIZE 1000000;
+DECLARE
+  TYPE t_name IS RECORD(
+     user_name users.username%TYPE,
+     role_name  roles.rolename%TYPE
+  );
+  r_name   t_name; -- name record
+  u_name users.username%TYPE := 200;
+BEGIN
+  SELECT first_name,
+         last_name
+  INTO r_name
+  FROM employees
+  WHERE employee_id = n_emp_id;
+  -- print out the employee's name
+  DBMS_OUTPUT.PUT_LINE(r_name.first_name || ',' || r_name.last_name );
+END;
+/
+
+declare
+    type t_users is record(
+        user_name users.fname%TYPE,
+        role_name roles.rolename%TYPE
+        );
+    cursor cur_chief is select u.fname,r.rolename from users u join roles r on u.roleid=r.roleid;
+    r_users t_users;
+begin
+    open cur_chief;
+    loop
+        fetch cur_chief into r_users;
+        exit when cur_chief%NOTFOUND;
+        dbms_output.put_line(r_users.user_name ||' - '||r_users.role_name);
+    end loop;
+    close cur_chief;
+end;
+/
+
+SET SERVEROUTPUT ON SIZE 1000000;
+DECLARE
+  CURSOR cur_emp IS
+    SELECT *
+    FROM employees
+    WHERE employee_id = 200;
+ 
+  emp_rec cur_emp%ROWTYPE;
+BEGIN
+  NULL;
+END;
+/
+
+SET serveroutput ON SIZE 1000000;
+DECLARE
+  TYPE t_name IS RECORD(
+    first_name employees.first_name%TYPE,
+    last_name employees.last_name%TYPE
+  );
+  r_name      t_name;
+  r_name2     t_name;
+  r_name_null t_name;
+  n_emp_id employees.employee_id%TYPE := 200;
+BEGIN
+  -- assign employee's infomation to record
+  SELECT first_name,
+         last_name
+  INTO r_name
+  FROM employees
+  WHERE employee_id = n_emp_id;
+ 
+  -- assign record to another record
+  r_name2 := r_name;
+  -- print out the employee's name
+  DBMS_OUTPUT.PUT_LINE(r_name2.first_name || ',' || r_name2.last_name);
+ 
+  -- assign record to NULL
+  r_name2 := r_name_null; 
+ 
+  -- check NULL for each individual field
+  IF r_name2.first_name IS NULL AND
+     r_name2.last_name IS NULL THEN
+    DBMS_OUTPUT.PUT_LINE('Record r_name2 is NULL');
+  END IF;
+ 
+END;
+/
+
+
+DECLARE
+  TYPE t_address IS RECORD(
+    house_number VARCHAR2(6),
+    street       VARCHAR2(50),
+    phone        VARCHAR2(15),
+    region       VARCHAR2(10),
+    postal_code  VARCHAR2(10),
+    country      VARCHAR2(25)
+  );
+ 
+  TYPE t_contact IS RECORD(
+    home     t_address,
+    business t_address
+  );
+  r_contact t_contact;
+BEGIN
+  r_contact.business.house_number := '500';
+  r_contact.business.street       := 'Oracle Parkway';
+  r_contact.business.region       := 'CA';
+  r_contact.business.postal_code  := '94065';
+  r_contact.business.country      := 'USA';
+  r_contact.business.phone        := '+1.800.223.1711';
+END;
 
 
 
